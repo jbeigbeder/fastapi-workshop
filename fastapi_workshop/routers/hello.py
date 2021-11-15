f98d20c8cffd54924b2ac8855ed7e98414049353
Jérome Beigbeder <jerome.beigbeder@hotmail.fr>
2021-11-15 17:44:10 +0100

    chore: Second enpoint say hello with an echo

--- fastapi_workshop/routers/hello.py
+++ fastapi_workshop/routers/hello.py
@@ -6,3 +6,16 @@ router = APIRouter()
 @router.get('/hello', tags=['hello'])
 def say_hello_world():
     return 'Hello world with FastAPI!'
+
+
+@router.get("/hello/{name}")
+async def say_hello(name: str, echo: int = 1):
+    """Say hello to someone
+
+    Parameters
+    ----------
+    name: name (required)
+    echo: nombre of time the API say hello (optional, default to 1)
+    """
+    message = "Hello " + name + "! "
+    return {"message": message * echo}
