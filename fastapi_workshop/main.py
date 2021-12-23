852a8373c325f3177f2a3e5d572c8ded6c7be73e
Jérôme Beigbeder <jerome.beigbeder@hotmail.fr>
2021-12-23 11:27:18 +0100

    fix: add correct title tothe webapp

--- fastapi_workshop/main.py
+++ fastapi_workshop/main.py
@@ -9,7 +9,7 @@ from .routers import hello, customer
 # Initialize SQL schema with metadatas of models
 customer_model.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title='Workshop: FastApi')
 
 # add all the APIRouter
 app.include_router(hello.router)
